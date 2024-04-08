4227d0cb679b09a0b464810062509e9de91faa35
Yulia <93128899+Aealius@users.noreply.github.com>
2024-04-08 12:03:27 +0300

    add exception handler to middleware pipeline

--- OnlineStore.UI/Program.cs
+++ OnlineStore.UI/Program.cs
@@ -1,6 +1,10 @@
+using OnlineStore.UI.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.UseCustomExceptionHandlerMiddleware();
+
 
 app.Run();
