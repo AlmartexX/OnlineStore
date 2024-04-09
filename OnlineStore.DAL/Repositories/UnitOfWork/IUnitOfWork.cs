108eb04aeb0bb18870aef38e9d67be05435faf46
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-04-09 18:17:23 +0300

    implementation-of-unitofwork-pattern

---
+++ OnlineStore.DAL/Repositories/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using OnlineStore.DAL.Repositories.Interfaces;
+
+namespace OnlineStore.DAL.Repositories.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        IProductRepository Products { get; }
+        ICategoryRepository Categories { get; }
+        IOrderRepository Orders { get; }
+        IOrderItemRepository OrderItems { get; }
+        IUserRepository Users { get; }
+
+        Task SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
