0a10f6799bb3606b746d7465f99a89894e53f336
Vadim <vadim565162@mail.ru>
2024-04-09 12:36:29 +0300

    Create entity models, context, entitys configuration

--- OnlineStore.BLL/obj/Debug/net6.0/OnlineStore.BLL.AssemblyInfo.cs
+++ OnlineStore.BLL/obj/Debug/net6.0/OnlineStore.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineStore.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af47bfa4c9002443431b264f75ca5818bc251ba6")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineStore.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineStore.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ OnlineStore.DAL/Context/EntityConfiguration/OrderConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineStore.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.DAL.Context.EntityConfiguration
+{
+    internal class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+
+            builder.HasKey(c => c.Id);
+            builder.HasIndex(c => c.Id).IsUnique();
+
+            builder.HasOne(d => d.User).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_Order_UserId");
+
+        }
+    }
+}
