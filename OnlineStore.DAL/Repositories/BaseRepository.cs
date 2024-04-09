9d8b80c936c4834063f0ca43b35f41089c028cc6
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-04-09 18:11:50 +0300

    adding-base-repository

---
+++ OnlineStore.DAL/Repositories/BaseRepository.cs
@@ -0,0 +1,43 @@
+﻿using OnlineStore.DAL.Repositories.Interfaces;
+
+namespace OnlineStore.DAL.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity>
+    {
+        protected readonly OnlineStoreDbContext _context;
+
+        public BaseRepository(OnlineStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(TEntity item, CancellationToken cancellationToken)
+        {
+            await _context.AddAsync(item, cancellationToken);
+        }
+
+        public async Task DeleteAsync(int id, CancellationToken cancellationToken)
+        {
+            var entityToDelete = await _context.FindAsync<TEntity>(id, cancellationToken);
+            _context.Remove(entityToDelete);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken)
+        {
+            return await _context.Set<TEntity>()
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            return await _context.FindAsync<TEntity>(id, cancellationToken);
+        }
+
+        public async Task UpdateAsync(int id, TEntity item, CancellationToken cancellationToken)
+        {
+            var entityToUpdate = await _context.FindAsync<TEntity>(id, cancellationToken);
+            _context.Entry<TEntity>(entityToUpdate).CurrentValues.SetValues(item);
+        }
+    }
+}
---
+++ OnlineStore.DAL/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineStore.DAL.Repositories.Interfaces
+{
+    public interface IBaseRepository<TEntity>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken);
+        Task<TEntity> GetByIdAsync(int id, CancellationToken cancellationToken);
+        Task AddAsync(TEntity item, CancellationToken cancellationToken);
+        Task UpdateAsync(int id, TEntity item, CancellationToken cancellationToken);
+        Task DeleteAsync(int id, CancellationToken cancellationToken);
+    }
+}
