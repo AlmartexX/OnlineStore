9f56114ec3c2a049e63fce7b90abe5cfdade1289
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-04-10 17:11:57 +0300

    adding-repository-wrapper-to-DI

--- OnlineStore.UI/Program.cs
+++ OnlineStore.UI/Program.cs
@@ -1,4 +1,9 @@
+using OnlineStore.UI.ServiceCollection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigureRepositoryWrapper();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ OnlineStore.UI/ServiceCollection/ServiceCollectionExtension.cs
@@ -0,0 +1,14 @@
+﻿using OnlineStore.DAL.Repositories.UnitOfWork;
+
+namespace OnlineStore.UI.ServiceCollection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection ConfigureRepositoryWrapper(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
