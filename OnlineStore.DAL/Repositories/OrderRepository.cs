e94b72f749f05f59aee2dfd2acf4075408748966
Dzmitry <tarasovets.dmtiv@gmail.com>
2024-04-10 18:30:26 +0300

    feat: add Repositories

---
+++ OnlineStore.DAL/Repositories/OrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using OnlineStore.DAL.Repositories.Interfaces;
+
+namespace OnlineStore.DAL.Repositories
+{
+    public class OrderRepository : BaseRepository<Order>, IOrderRepository
+    {
+        public Order(OnlineStoreDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ OnlineStore.DAL/Repositories/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using OnlineStore.DAL.Repositories.Interfaces;
+
+namespace OnlineStore.DAL.Repositories
+{
+    public class ProductRepository : BaseRepository<Product>, IProductRepository
+    {
+        public ProductRepository(OnlineStoreDbContext context) : base(context)
+        {
+
+        }
+
+        public async Task<Product> FindProductByCategory(string categoryName ,CancellationToken cancellationToken)
+        {
+            return await _context.Products
+                .Where(x => x.CategoryName == categoryName).ToListAsync();
+        }
+    }
+}
