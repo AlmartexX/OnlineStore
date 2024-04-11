ee51c986879773b1be1f930c6dd8edde84a79ece
zalevskayaa.anya@gmail.com <zalevskayaa.anya@gmail.com>
2024-04-11 18:05:59 +0300

    adding-validators-for-create-models

---
+++ OnlineStore.BLL/Vallidation/CreateCategoryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace OnlineStore.BLL.Vallidation
+{
+    public class CreateCategoryValidator : AbstractValidator<CreateCategoryDTO>
+    {
+        public CreateCategoryValidator() 
+        {
+            RuleFor(user => user.Title)
+                .NotEmpty()
+                .Length(2, 12)
+                .WithMessage("Title must be between 2 and 12 characters.");
+        }
+    }
+}
---
+++ OnlineStore.BLL/Vallidation/CreateProductValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace OnlineStore.BLL.Vallidation
+{
+    public class CreateProductValidator : AbstractValidator<CreateProductDTO>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(product => product.Title)
+                .NotEmpty()
+                .Length(2, 12)
+                .WithMessage("Title must be between 2 and 12 characters.");
+
+            RuleFor(product => product.Price)
+                .NotEmpty()
+                .GreaterThan(0)
+                .WithMessage("Product can't be free. Price must be greater than zero.");
+
+            RuleFor(item => item.CategoryId)
+                .NotEmpty()
+                .GreaterThan(0)
+                .WithMessage("Category id must not be changed!");
+        }
+    }
+}
