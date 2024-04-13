3f22171a7c24df5171195e175f26aacbdfc18710
VadzimChumakou <vadim565162@mail.ru>
2024-04-13 21:47:32 +0300

    Add entities configuration

---
+++ OnlineStore.DAL/Context/EntitiesConfiguration/OrderConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineStore.DAL.Context.Entities;
+
+namespace OnlineStore.DAL.Context.EntitiesConfiguration
+{
+    internal class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+
+            builder.HasKey(c => c.Id);
+            builder.HasIndex(c => c.Id).IsUnique();
+
+            builder.HasOne(d => d.User).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_Order_UserId");
+
+        }
+    }
+}
---
+++ OnlineStore.DAL/Context/EntitiesConfiguration/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineStore.DAL.Context.Entities;
+
+namespace OnlineStore.DAL.Context.EntitiesConfiguration
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.HasIndex(c => c.Id).IsUnique();
+
+        }
+    }
+}
