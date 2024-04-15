b1cea5aa29797fb062fa0c4f04905342693e98ef
Alex <ymka.alexmartunov@gmail.com>
2024-04-15 10:51:09 +0300

    Add Category and Product Controller

---
+++ OnlineStore.UI/Controllers/CategoryController.cs
@@ -0,0 +1,69 @@
+﻿using OnlineStore.BLL.Services.Interfaces;
+using OnlineStore.BLL.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace OnlineStore.UI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpPost]
+        [SwaggerOperation("Create a new category")]
+        [ProducesResponseType(typeof(CreateCategoryDTO), 200)]
+        public async Task<ActionResult<CreateCategoryDTO>> CreateCategoryAsync(CreateCategoryDTO newCategory, CancellationToken cancellationToken)
+        {
+            var createdCategory = await _categoryService.CreateCategoryAsync(newCategory, cancellationToken);
+
+            return Ok(createdCategory);
+        }
+
+        [HttpGet]
+        [SwaggerOperation("Get categories")]
+        [ProducesResponseType(typeof(IEnumerable<CategoryDTO>), 200)]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetCategoriesAsync([FromQuery] PaginationSettingsDTO paginationSettings, CancellationToken cancellationToken)
+        {
+            var categories = await _categoryService.GetCategoriesAsync(paginationSettings, cancellationToken);
+
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        [SwaggerOperation("Get category by ID")]
+        [ProducesResponseType(typeof(CategoryDTO), 200)]
+        public async Task<ActionResult<CategoryDTO>> GetCategoryByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            var category = await _categoryService.GetCategoryByIdAsync(id, cancellationToken);
+
+            return Ok(category);
+        }
+
+        [HttpPut("{id}")]
+        [SwaggerOperation("Update a category")]
+        [ProducesResponseType(typeof(CategoryDTO), 200)]
+        public async Task<ActionResult<CategoryDTO>> UpdateCategoryAsync(int id, CategoryDTO categoryDTO, CancellationToken cancellationToken)
+        {
+            var updatedCategory = await _categoryService.UpdateCategoryAsync(categoryDTO, id, cancellationToken);
+
+            return Ok(updatedCategory);
+        }
+
+        [HttpDelete("{id}")]
+        [SwaggerOperation("Delete a category")]
+        [ProducesResponseType(typeof((bool, string)), 200)]
+        public async Task<ActionResult<(bool, string)>> DeleteCategoryAsync(int id, CancellationToken cancellationToken)
+        {
+            var result = await _categoryService.DeleteCategoryAsync(id, cancellationToken);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ OnlineStore.UI/Controllers/ProductController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.BLL.Services.Interfaces;
+using OnlineStore.BLL.DTO;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace OnlineStore.UI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpPost]
+        [SwaggerOperation("Create a new product")]
+        [ProducesResponseType(typeof(ProductDTO), 200)]
+        public async Task<ActionResult<ProductDTO>> CreateProductAsync(ProductDTO newProduct, CancellationToken cancellationToken)
+        {
+            var createdProduct = await _productService.CreateProductAsync(newProduct, cancellationToken);
+
+            return Ok(createdProduct);
+        }
+
+        [HttpGet]
+        [SwaggerOperation("Get products")]
+        [ProducesResponseType(typeof(IEnumerable<ProductDTO>), 200)]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetProductsAsync([FromQuery] PaginationSettingsDTO paginationSettings, CancellationToken cancellationToken)
+        {
+            var products = await _productService.GetProductsAsync(paginationSettings, cancellationToken);
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        [SwaggerOperation("Get product by ID")]
+        [ProducesResponseType(typeof(ProductDTO), 200)]
+        public async Task<ActionResult<ProductDTO>> GetProductByIdAsync(int id, CancellationToken cancellationToken)
+        {
+            var product = await _productService.GetProductByIdAsync(id, cancellationToken);
+
+            return Ok(product);
+        }
+
+        [HttpGet("category/{categoryName}")]
+        [SwaggerOperation("Get product by category")]
+        [ProducesResponseType(typeof(ProductDTO), 200)]
+        public async Task<ActionResult<ProductDTO>> GetProductByCategoryAsync(string categoryName, CancellationToken cancellationToken)
+        {
+            var product = await _productService.GetProductByCategoryAsync(categoryName, cancellationToken);
+
+            return Ok(product);
+        }
+
+        [HttpPut("{id}")]
+        [SwaggerOperation("Update a product")]
+        [ProducesResponseType(typeof(ProductDTO), 200)]
+        public async Task<ActionResult<ProductDTO>> UpdateProductAsync(int id, ProductDTO productDTO, CancellationToken cancellationToken)
+        {
+            var updatedProduct = await _productService.UpdateProductAsync(productDTO, id, cancellationToken);
+
+            return Ok(updatedProduct);
+        }
+
+        [HttpDelete("{id}")]
+        [SwaggerOperation("Delete a product")]
+        [ProducesResponseType(typeof((bool, string)), 200)]
+        public async Task<ActionResult<(bool, string)>> DeleteProductAsync(int id, CancellationToken cancellationToken)
+        {
+            var result = await _productService.DeleteProductAsync(id, cancellationToken);
+
+            return Ok(result);
+        }
+    }
+}
