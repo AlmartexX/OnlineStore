d7a848d1103fa807ef4867436add18b4b66b5315
VadzimChumakou <vadim565162@mail.ru>
2024-04-15 04:26:38 +0300

    Add Tocken generation

---
+++ OnlineStore.BLL/Services/JwtTokenProvider.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using OnlineStore.BLL.JwtInfrastructure.Models;
+
+namespace OnlineStore.BLL.Services
+{
+    public class JwtTokenProvider
+    {
+        private readonly TokenOptions _options;
+        public JwtProvider(IOptions<TokenOptions> options)
+        {
+            _options = options.Value;
+        }
+        public string GenerateToken(User user)
+        {
+            Claim[] claims = [new("UserId", user.Id.ToString())];
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey)), SecurityAlgorithms.HmacSha256),
+                expires: DateTime.UtcNow.AddHours(_options.ExpitesHours)
+                );
+            var tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+            return tokenValue;
+        }
+    }
+}
