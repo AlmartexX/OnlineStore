c28ed23e7157dce1b0ac2a252d8cc676db2bbcbb
VadzimChumakou <vadim565162@mail.ru>
2024-04-15 06:07:46 +0300

    Add swager configuration for auth

--- OnlineStore.UI/ServiceCollection/ServiceCollectionExtension.cs
+++ OnlineStore.UI/ServiceCollection/ServiceCollectionExtension.cs
@@ -54,5 +54,36 @@ namespace OnlineStore.UI.ServiceCollection
             return services;
         }
 
+        public static IServiceCollection ConfigureSwagerForJwt(this IServiceCollection services)
+        {
+            services.ConfigureSwaggerGen(options =>
+             options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+             {
+                 Name = "Authorization",
+                 Type = SecuritySchemeType.ApiKey,
+                 Scheme = "Bearer",
+                 BearerFormat = "JWT",
+                 In = ParameterLocation.Header,
+                 Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and your token in the text input",
+             }));
+            services.ConfigureSwaggerGen(options =>
+             options.AddSecurityRequirement(new OpenApiSecurityRequirement
+             {
+                 {
+                       new OpenApiSecurityScheme
+                         {
+                             Reference = new OpenApiReference
+                             {
+                                 Type = ReferenceType.SecurityScheme,
+                                 Id = "Bearer"
+                             }
+                         },
+                         new string[] {}
+                 }
+             }));
+
+            return services;
+        }
+
     }
 }
