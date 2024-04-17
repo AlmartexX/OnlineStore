03050d2da6e967ae6c0b02403698914374e77977
Dzmitry <tarasovets.dmtiv@gmail.com>
2024-04-17 07:43:01 +0300

    feat: add AuthService

---
+++ OnlineStore.BLL/MappConfigs/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using OnlineStore.BLL.DTO.User;
+
+namespace OnlineStore.BLL.MappConfigs.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<TokenDto> LoginUserAsync(LoginUserDto dto, CancellationToken cancellationToken);
+        Task<UserDto> RegisterUserAsync(RegisterUserDto dto, CancellationToken cancellationToken);
+    }
+}
--- OnlineStore.DAL/obj/Debug/net6.0/OnlineStore.DAL.AssemblyInfo.cs
+++ OnlineStore.DAL/obj/Debug/net6.0/OnlineStore.DAL.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineStore.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+<<<<<<< HEAD
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8e2c4d32ad108abaa4f8a3ccb05575a17c9b573")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d51a1cb9213dc2d1f5b8a92eae2092439ce0c1d1")]
+>>>>>>> main
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineStore.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineStore.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
