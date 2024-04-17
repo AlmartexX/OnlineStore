3f4485b921e49e9a84fb4362a21f85231068ef71
Dzmitry <tarasovets.dmtiv@gmail.com>
2024-04-17 08:50:17 +0300

    add obj

--- OnlineStore.DAL/obj/Debug/net6.0/OnlineStore.DAL.AssemblyInfo.cs
+++ OnlineStore.DAL/obj/Debug/net6.0/OnlineStore.DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineStore.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fda9a1af0c598d4636e82bc82b3754f9d2bde6ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f58df30f5cb3cfa080b2ed4a8049a8ecbf437575")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineStore.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineStore.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
