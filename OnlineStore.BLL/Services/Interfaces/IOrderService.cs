f58df30f5cb3cfa080b2ed4a8049a8ecbf437575
Dzmitry <tarasovets.dmtiv@gmail.com>
2024-04-17 08:16:31 +0300

    add OrderService

---
+++ OnlineStore.BLL/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using OnlineStore.BLL.DTO;
+
+namespace OnlineStore.BLL.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<CreateOrderDto> CreateOrderAsync(CreateOrderDto orderDto, CancellationToken cancellationToken);
+        Task<IEnumerable<OrderDTO>> GetOrdersAsync(PaginationSettingsDTO paginationSettings, CancellationToken cancellationToken);
+
+        Task<IEnumerable<OrderDto>> GetOrdersByIdAsync(int id, PaginationSettingsDTO paginationSettings,
+            CancellationToken cancellationToken);
+
+        Task<OrderDto> GetOrderByIdIncludeOrderItemAsync(int id, CancellationToken cancellationToken);
+    }
+}
